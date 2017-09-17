d4fb28babf0e7e7ef5b8e94417fc62f73f036f1e
divastavisca <dagarwal@tavisca.com>
2017-09-17 16:32:10 +0530

    added primary custom language definitions

---
+++ DWPS.Definitions/Attributes/ElementAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DWPS.Definitions.Attributes
+{
+    public class ElementAttribute
+    {
+        public string Name { get; private set; }
+        public object Value { get; private set; }
+
+        public ElementAttribute(string name,object value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
---
+++ DWPS.Definitions/Controls/CustomServerControl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DWPS.Definitions.Attributes;
+
+namespace DWPS.Definitions.Controls
+{
+    public class CustomServerControl
+    {
+        public string Name { get; }
+
+        public List<ElementAttribute> ControlAttributes;
+
+        public override string ToString()
+        {
+            return this.getLanguageRepresentation();
+        }
+
+        private string getLanguageRepresentation()
+        {
+            StringBuilder actualControlScript = new StringBuilder();
+            actualControlScript.Append("<dwps:");
+            actualControlScript.Append(this.Name+" ");
+            foreach(ElementAttribute controlAttribute in ControlAttributes)
+            {
+                actualControlScript.Append(controlAttribute.Name);
+                actualControlScript.Append("=");
+                actualControlScript.Append(controlAttribute.Value.ToString());
+                if(ControlAttributes.IndexOf(controlAttribute)!=ControlAttributes.Count-1)
+                    actualControlScript.Append(" ");
+            }
+            actualControlScript.Append("></dwps:");
+            actualControlScript.Append(this.Name);
+            actualControlScript.Append(">");
+            return actualControlScript.ToString();
+        }
+    }
+}
